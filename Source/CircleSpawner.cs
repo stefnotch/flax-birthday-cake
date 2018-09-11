ffe1a5ed8f6e8a90f134e32a437e134dfed8ed44
Stefnotch <brandmairstefan@gmail.com>
2018-09-11 18:58:05 +0200

    CircleSpawner improvement

--- Source/CircleSpawner.cs
+++ Source/CircleSpawner.cs
@@ -1,20 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using FlaxEngine;
+using FlaxEngine.Utilities;
 
 namespace NinaBirthday
 {
 	[ExecuteInEditMode]
 	public class CircleSpawner : Script
 	{
+		private static Random _rng = new Random();
+		private float _radius;
+		private int _count;
+		private float _randomOffsetRadius;
+
 		public Prefab PrefabToSpawn { get; set; }
 		public Actor Container;
 
 		[Range(0, 1000)]
-		public float Radius
+		public float CircleRadius
 		{
 			get => _radius;
 			set { _radius = value; Spawn(); }
+		}
+
+		[Range(0, 100)]
+		public float RandomOffsetRadius
+		{
+			get => _randomOffsetRadius;
+			set { _randomOffsetRadius = value; Spawn(); }
 		}
 
 		[Range(0, 100)]
@@ -23,10 +36,6 @@ namespace NinaBirthday
 			get => _count;
 			set { _count = value; Spawn(); }
 		}
-
-		private float _radius;
-		private int _count;
-
 
 		private void OnEnable()
 		{
@@ -38,7 +47,7 @@ namespace NinaBirthday
 		{
 			if (PrefabToSpawn == null) return;
 
-			if (Container == null)
+			if (Container == null || Container.HasChildren)
 			{
 				Container = New<EmptyActor>();
 				this.Actor.AddChild(Container, false);
@@ -52,7 +61,8 @@ namespace NinaBirthday
 			for (int i = 0; i < Count; i++)
 			{
 				var spawnedActor = PrefabManager.SpawnPrefab(PrefabToSpawn, Container);
-				spawnedActor.LocalPosition += Vector3.Forward * Quaternion.RotationY(angle) * Radius;
+				spawnedActor.LocalPosition += Vector3.Forward * Quaternion.RotationY(angle) * CircleRadius;
+				spawnedActor.LocalPosition += _rng.NextVector3() * RandomOffsetRadius;
 				//spawnedActor.LookAt(this.Actor);
 				angle += Mathf.TwoPi / Count;
 			}
