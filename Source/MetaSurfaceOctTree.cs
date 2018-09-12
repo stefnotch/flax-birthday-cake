fec3b2b6eb3f29f30b2c069b7643dd6e774aef78
Stefnotch <brandmairstefan@gmail.com>
2018-09-12 12:22:01 +0200

    MetaSurfaceOctTree done

--- Source/MetaSurfaceOctTree.cs
+++ Source/MetaSurfaceOctTree.cs
@@ -16,18 +16,8 @@ namespace NinaBirthday.MetaSurf
 
 	public class MetaSurfaceOctTree
 	{
-		public static int MinElementsToSubdivide = 1;
-
-		public BoundingBox BoundingBox { get; private set; }
-
 		private MetaSurfaceOctTree[] _children;
 		private List<MetaSurfaceCell> _elements;
-
-		public MetaSurfaceOctTree(BoundingBox boundingBox)
-		{
-			BoundingBox = boundingBox;
-			_elements = new List<MetaSurfaceCell>();
-		}
 
 		public MetaSurfaceOctTree(BoundingBox boundingBox, List<MetaSurfaceCell> elements = null)
 		{
@@ -43,16 +33,58 @@ namespace NinaBirthday.MetaSurf
 			RecursiveSubdivide();
 		}
 
-		public void AddBoundingBox(BoundingBox boundingBox)
+		public static int MinElementsToSubdivide = 1;
+
+		public BoundingBox BoundingBox { get; private set; }
+
+		public bool IsEmpty => _elements.Count == 0;
+
+
+		private int _boundingBoxCount = 0;
+
+		/// <summary>
+		/// Used for adding an <see cref="ImplicitShape"/>
+		/// </summary>
+		public void AddBoundingBox(ref BoundingBox boundingBox)
 		{
 			//Subdivide
+
+			if (BoundingBox.Intersects(ref boundingBox))
+			{
+				_boundingBoxCount++;
+
+				Subdivide(ref boundingBox);
+
+				foreach (var child in _children)
+				{
+					AddBoundingBox(ref boundingBox);
+				}
+			}
 		}
 
-		public void RemoveBoundingBox(BoundingBox boundingBox)
+		/// <summary>
+		/// Used for removing an <see cref="ImplicitShape"/>
+		/// </summary>
+		public void RemoveBoundingBox(ref BoundingBox boundingBox)
 		{
 			if (_children.Length == 0) return;
 
-			//Un-subdivide
+			if (BoundingBox.Intersects(ref boundingBox))
+			{
+				_boundingBoxCount--;
+
+
+				//Un-subdivide
+				for (int i = 0; i < _children.Length; i++)
+				{
+					_children[i].RemoveBoundingBox(ref boundingBox);
+					if (_children[i]._boundingBoxCount <= 0)
+					{
+						// Remove child
+						_children[i] = null;
+					}
+				}
+			}
 		}
 
 		/*public MetaSurfaceCell this[int x, int y, int z]
@@ -63,11 +95,10 @@ namespace NinaBirthday.MetaSurf
 			}
 		}*/
 
-		public bool IsEmpty => _elements.Count == 0;
-
 		private void RecursiveSubdivide()
 		{
-			if (Subdivide())
+			var bb = BoundingBox;
+			if (Subdivide(ref bb))
 			{
 				foreach (var child in _children)
 				{
@@ -77,10 +108,10 @@ namespace NinaBirthday.MetaSurf
 		}
 
 		/// <summary>
-		/// Subdivies a cell into 8 children
+		/// Subdivies a cell into 0-8 children
 		/// </summary>
 		/// <returns>If it succeeded</returns>
-		public bool Subdivide()
+		public bool Subdivide(ref BoundingBox boundingBox)
 		{
 			if (_elements.Count <= 0) return false;
 			if (_elements.Count <= MinElementsToSubdivide) return false;
@@ -89,27 +120,46 @@ namespace NinaBirthday.MetaSurf
 			Vector3 center = BoundingBox.Center;
 			Vector3[] corners = BoundingBox.GetCorners();
 
-			var children = new List<MetaSurfaceOctTree>(corners.Length);
+			var children = new MetaSurfaceOctTree[corners.Length];
 
 			for (int i = 0; i < corners.Length; i++)
 			{
 				Vector3 min = Vector3.Min(corners[i], center);
 				Vector3 max = Vector3.Max(corners[i], center);
-				children[i] = new MetaSurfaceOctTree(new BoundingBox(min, max));
+
+				BoundingBox childBB = new BoundingBox(min, max);
+				if (childBB.Intersects(boundingBox))
+				{
+					children[i] = new MetaSurfaceOctTree(childBB);
+				}
 			}
 
+			int childCount = 8;
 			// And set their vertices
-			foreach (var child in children)
+			for (int i = 0; i < _children.Length; i++)
 			{
+				var child = _children[i];
+				if (child == null)
+				{
+					childCount--;
+					continue;
+				}
+
 				foreach (var cell in _elements)
 				{
 					if (child.AddElement(cell)) break;
 				}
+
+				if (child.IsEmpty)
+				{
+					childCount--;
+					_children[i] = null;
+				}
 			}
 
-			_children = children.Where(c => !c.IsEmpty).ToArray();
+			_children = children;
 
-			return _children.Length > 0;
+			return childCount > 0;
 		}
 
 		/// <summary>
