07769b7ecebc30391414e48289d8216f6bf2c127
Stefnotch <brandmairstefan@gmail.com>
2019-02-06 09:27:50 +0100

    Fix CakeCamera

--- Source/CakeCamera.cs
+++ Source/CakeCamera.cs
@@ -1,6 +1,6 @@
-﻿using FlaxEngine;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using FlaxEngine;
 
 namespace NinaBirthday
 {
@@ -8,8 +8,8 @@ namespace NinaBirthday
 	{
 		public Camera Camera { get; set; }
 
-		public float Min = 10;
-		public float Max = 120;
+		public float Min = -88;
+		public float Max = 88;
 		public Vector2 Speed = new Vector2(6);
 		public float Damping = 0.01f;
 		private const float Smoothing = 0.5f;
@@ -32,7 +32,7 @@ namespace NinaBirthday
 		{
 			if (Input.GetMouseButton(MouseButton.Right))
 			{
-				_velocity += new Vector2(-Input.MousePositionDelta.Y, Input.MousePositionDelta.X);
+				_velocity += new Vector2(Input.MousePositionDelta.Y, Input.MousePositionDelta.X);
 			}
 		}
 
@@ -44,15 +44,11 @@ namespace NinaBirthday
 			_angle += _velocity * Speed * Time.DeltaTime;
 			_angle.X = Mathf.Clamp(_angle.X, Min, Max);
 
-			Vector3 cameraOffsetVector = Vector3.Up * Quaternion.Euler(_angle.X, _angle.Y, 0);
+			Quaternion cameraOrientation = Quaternion.Euler(_angle.X, _angle.Y, 0);
+			Camera.LocalOrientation = Quaternion.Slerp(Camera.LocalOrientation, cameraOrientation, Smoothing);
 
-			Vector3 cameraPosition = cameraOffsetVector * _distance;
-
-			Vector3 lerpedCameraPosition = Vector3.Lerp(Camera.LocalPosition, cameraPosition, Smoothing);
-			lerpedCameraPosition *= _distance / lerpedCameraPosition.Length;
-
-			Camera.LocalPosition = lerpedCameraPosition;
-			Camera.LookAt(Camera.Parent.Position);
+			Vector3 cameraPosition = (Vector3.Backward * _distance) * cameraOrientation;
+			Camera.LocalPosition = Vector3.Lerp(Camera.LocalPosition, cameraPosition, Smoothing);
 		}
 	}
 }
